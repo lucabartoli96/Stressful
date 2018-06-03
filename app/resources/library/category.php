51b4305941322e25102a11e061e2790f6de17407
lucabartoli96 <lucabartoli1996@gmail.com>
2018-06-03 21:10:00 +0200

    Add files

---
+++ app/resources/library/category.php
@@ -0,0 +1,71 @@
+
+<?php
+
+class Category {
+    
+    private static $ALL = "SELECT * FROM category";
+    private static $CHECK = "SELECT * FROM category WHERE name=%s LIMIT 1";
+    private static $INSERT = "INSERT INTO category (name, creator) VALUES ('%s', '%s')";
+    private static $DELETE = "DELETE FROM category WHERE name='%s'";
+    
+    private static $instance = null;
+
+    private $db = null;
+    private $user = null;
+    
+    private function __construct() { }
+    
+    public static function get() {
+        
+        Session::start();
+        
+        if ( !isset($_SESSION[__CLASS__]) ) {
+            $_SESSION[__CLASS__] = new static();
+        }
+        
+        return $_SESSION[__CLASS__];
+        
+    }
+    
+    public function all() {
+        $db = Connection::get();
+        return $db->query($ALL);
+    }
+    
+    public function add($name) {
+        
+        $user = User::get();
+        
+        if( $user->is_admin() ) {
+         
+            $db = Connection::get();
+            $category = $db->query(sprintf(self::$CHECK, $name))->fetch_assoc();
+        
+            if( $category ) {
+                throw new CategoryException("$name already exists!");
+            } else {
+                $db->query(sprintf(self::$INSERT, $name, $creator));
+            }
+            
+        } else {
+            throw new UserException('User does not have enough privilieges to add a test');
+        }
+        
+    }
+    
+    public function delete($name) {
+            
+        $user = User::get();
+        
+        if( $user->is_admin() ) {
+            
+            $db = Connection::get();
+            $db->query(sprintf(self::$DELETE, $name));
+            
+        } else {
+            throw new UserException('User does not have enough privilieges to delete a test');
+        }
+    }
+}
+
+?>
---
+++ app/resources/library/exceptions.php
@@ -0,0 +1,49 @@
+<?php
+
+class UserException extends Exception {
+    private $stack;
+    
+    public function __construct($message) {
+        parent::__construct($message);
+        $this->stack = null;
+    }
+    
+    public function err($name) {
+        return isset($this->stack[$name]);
+    }
+    
+    public function push($name, $message) {
+        
+        if ( !$this->is_set() ) {
+            $this->stack = array();
+        }
+        
+        if ( !$this->err($name) ) {
+            $this->stack[$name] = array();
+        }
+        
+        array_push($this->stack[$name], $message);
+    }
+    
+    public function get($name) {
+        if ( $this->err($name) ) {
+            return $this->stack[$name];
+        } else {
+            return null;
+        }
+    }
+    
+    public function is_set() {
+        return $this->stack !== null;
+    }
+}
+
+class CategoryException {
+
+    public function __construct($message) {
+        parent::__construct($message);
+    }
+}
+
+
+?>
